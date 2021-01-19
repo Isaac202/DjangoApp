f48eb5406c53d87479c82c555a379d68cd164f21
Isaac de Oliveira <isaacbyguaracity@gmail.com>
2021-01-19 17:13:39 -0300

    Fim da primeira etapa do projeto

--- recits/settings.py
+++ recits/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'recits.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'receita',
+        'USER': 'postgres',
+        'PASSWORD': '19971910',
+        'HOST': 'localhost'
     }
 }
 
